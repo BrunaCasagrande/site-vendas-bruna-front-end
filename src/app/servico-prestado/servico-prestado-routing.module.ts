31790ea70fa6a6c8e140c79f5b784dda21325298
BrunaCasagrande <brunaczaramela@gmail.com>
2022-05-27 10:34:02 -0300

    primeiro commit do front-end do site-de-vendas-bruna

---
+++ src/app/servico-prestado/servico-prestado-routing.module.ts
@@ -0,0 +1,15 @@
+import { ServicoPrestadoListaComponent } from './servico-prestado-lista/servico-prestado-lista.component';
+import { ServicoPrestadoFormComponent } from './servico-prestado-form/servico-prestado-form.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'servico-prestado-form', component: ServicoPrestadoFormComponent },
+  { path: 'servico-prestado-listagem', component: ServicoPrestadoListaComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ServicoPrestadoRoutingModule { }
